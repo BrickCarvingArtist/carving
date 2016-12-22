ba8cc6faef21ab210404cef6cf7cc5b8d664945b
pengcheng.yang <806321554@qq.com>
2016-12-22 22:41:37 +0800

    optimize the denied paths validate

--- router.js
+++ router.js
@@ -14,20 +14,17 @@ const writeFile = promisify(fs.writeFile);
 const readNginxConf = async path => {
 	let file;
 	try{
-		if(!ValidFilePath.includes(path)){
-			throw new Error("无权访问此文件");
-		}
 		file = await readFile(`${FilePathPrefix}${path}.conf`, "utf-8");
 	}catch(e){
 		file = e.toString().replace(/.*:(.*)/, "$1");
 	}
 	return file;
 };
-const writeNginxConf = async (path, file) => {
+const writeNginxConf = async (path, file, validFilePath) => {
 	let code,
 		message;
 	try{
-		if(!ValidFilePath.includes(path)){
+		if(!validFilePath.includes(path)){
 			throw new Error("无权修改此文件");
 		}
 		await writeFile(`${FilePathPrefix}${path}.conf`, file);
@@ -42,12 +39,35 @@ const writeNginxConf = async (path, file) => {
 		message
 	};
 };
-const getHTML = async path => (await readFile("./modifier.html", "utf-8")).replace(/(<(textarea).*>)(<\/\2>)/, "$1" + await readNginxConf(path) + "$3");
+const getHTML = async (path, validFilePath) => {
+	try{
+		if(!validFilePath.includes(path)){
+			throw new Error("无权访问此文件");
+		}
+		return (await readFile("./modifier.html", "utf-8")).replace(/(<(textarea).*>)(<\/\2>)/, "$1" + await readNginxConf(path) + "$3");
+	}catch(e){
+		return e.toString().replace(/.*:(.*)/, "$1");
+	}
+};
 export default new Router()
-	.get("/", async ({query, response}) => {
-		response.body = await getHTML(query.path);
+	.get("/23336666/:path", async ({params, response}) => {
+		response.body = await getHTML(params.path, ValidFilePath);
+	})
+	.get("/guanjunpeng/:path", async ({params, response}) => {
+		response.body = await getHTML(params.path, ValidFilePath.filter(item => item.includes("20160912")));
+	})
+	.get("/xieyinping/:path", async ({params, response}) => {
+		response.body = await getHTML(params.path, ValidFilePath.filter(item => item.includes("20161010")));
 	})
 	.post("/update", async ({request, query, response}) => {
-		response.body = await writeNginxConf(query.path, request.body.file);
+		const {
+			user,
+			path
+		} = query;
+		response.body = await writeNginxConf(path, request.body.file, ValidFilePath.filter(item => item.includes({
+			23336666 : item,
+			guanjunpeng : "20160912",
+			xieyinping : "20161010"
+		}[user])));
 	})
 	.routes();
