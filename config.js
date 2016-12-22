29e724dea6cbf9f89e18fbc6e5a69cf265171c58
pengcheng.yang <806321554@qq.com>
2016-12-22 21:17:49 +0800

    add the denied file paths

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ export const ServerConfig = {
 	port : 12345
 };
 export const ValidFilePath = [
-	"livedemos.conf",
-	"20160704.xihu.conf",
-	"20160801.xihu.conf",
-	"20160912.xihu.conf", 
-	"20160912.xiasha.conf",
-	"20160912.qinhuai.conf",
-	"20161010.xihu.conf",
-	"20161010.xiasha.conf",
-	"20161010.putuo.conf"
-];
+	"livedemos",
+	"20160704.xihu",
+	"20160801.xihu",
+	"20160912.xihu", 
+	"20160912.xiasha",
+	"20160912.qinhuai",
+	"20161010.xihu",
+	"20161010.xiasha",
+	"20161010.putuo"
+].map(item => `/usr/local/nginx/conf/${item}.conf`);
