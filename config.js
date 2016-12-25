8731545a13e005b678ea99f1f7661080253a6299
BrickCarvingArtist <806321554@qq.com>
2016-12-25 19:05:59 +0800

    wtf?

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ export const SERVER_CONFIG = {
 	host : "127.0.0.1",
 	port : 12345
 };
-export const NGINX_CONF_PATH_PREFIX = ["/usr/local/nginx/conf/", "./nginx/"][isDevelopment];
-export const GIT_REPOSITORY_PATH_PREFIX = ["/root/H5/", "./H5/"][isDevelopment];
+export const NGINX_CONF_PATH_PREFIX = ["/usr/local/nginx/conf/", "/Users/apple/Desktop/carving/nginx/"][isDevelopment];
+export const GIT_REPOSITORY_PATH_PREFIX = ["/root/H5/", "/Users/apple/Desktop/carving/H5/"][isDevelopment];
 export const VALID_TIME_AREA = {
 	20160704 : ["xihu"],
 	20160801 : ["xihu"],
