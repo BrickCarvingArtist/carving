3b407900b23f57e399e977b44efbda776b39f88e
pengcheng.yang <806321554@qq.com>
2016-12-27 00:30:56 +0800

    upgrade the user experience

--- git.js
+++ git.js
@@ -3,12 +3,12 @@ import {spawn} from "child_process";
 export default app => {
 	const git = new IO("git");
 	git.attach(app);
-	git.on("ack", async ({acknowledge}, id) => {
+	git.on("ack", async ({socket, acknowledge}, id) => {
 		acknowledge("the file accepted.");
 		const gitsh = spawn(`./git.${id}.sh`);
-		gitsh.stdout.on("data", data => git.broadcast("message", data.toString()));
-		gitsh.stderr.on("data", data => git.broadcast("message", data.toString()));
-		gitsh.on("close", () => git.broadcast("message", "all repositories was published."));
+		gitsh.stdout.on("data", data => socket.broadcast("message", data.toString()));
+		gitsh.stderr.on("data", data => socket.broadcast("message", data.toString()));
+		gitsh.on("close", () => socket.broadcast("message", "all repositories was published."));
 	});
 	return app;
 };
