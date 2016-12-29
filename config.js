0d3e3f4b38038c919e0fc7d89d57efdfb1781911
pengcheng.yang <806321554@qq.com>
2016-12-29 16:09:30 +0800

    change to the right path

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const isDevelopment = +(process.env.NODE_ENV === "development");
 export const NGINX_CONF_PATH_PREFIX = ["/usr/local/nginx/conf/", "/Users/apple/Desktop/carving/output/"][isDevelopment];
-export const OUTPUT_FILES_PATH_PREFIX = ["/Root/carving/output/", "/Users/apple/Desktop/carving/output/"][isDevelopment];
+export const OUTPUT_FILES_PATH_PREFIX = ["/root/carving/output/", "/Users/apple/Desktop/carving/output/"][isDevelopment];
 export const GIT_REPOSITORY_PATH_PREFIX = ["/root/H5/", "/Users/apple/Desktop/carving/H5/"][isDevelopment];
 export const VALID_TIME_AREA = {
 	20160704 : ["xihu"],
