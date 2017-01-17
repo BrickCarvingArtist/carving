ce3908e8ed8767616e1187e5c0ef4f6a9d1dde64
pengcheng.yang <806321554@qq.com>
2017-01-17 22:15:54 +0800

    add eslint to lint js code

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
